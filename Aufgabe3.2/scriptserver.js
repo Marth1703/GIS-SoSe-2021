afbf8ca25c8ac1702ae0f7b94001fed224aa215f
Marth1703 <marvin.fischer@hs-furtwangen.de>
2021-06-06 16:47:25 +0200

    final version

--- Aufgabe3.2/scriptserver.js
+++ Aufgabe3.2/scriptserver.js
@@ -1,17 +1,12 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Aufgabe3_2 = void 0;
 var Aufgabe3_2;
 (function (Aufgabe3_2) {
-    document.getElementById("html").addEventListener("click", test);
     document.getElementById("html").addEventListener("click", clickHtml);
     document.getElementById("json").addEventListener("click", clickJson);
-    //htmlButton.addEventListener("click", handleHTML);
-    //jsonButton.addEventListener("click", handleJSON);
-    let url = "https://marthgissose2021.herokuapp.com/";
-    let div = document.createElement("div");
-    document.body.appendChild(div);
+    let url;
+    let div = document.getElementById("anzeige");
     async function clickHtml() {
+        url = "https://marthgissose2021.herokuapp.com/";
         let formData = new FormData(document.forms[0]);
         let query = new URLSearchParams(formData);
         url = url + "/html?" + query.toString();
@@ -22,6 +17,7 @@ var Aufgabe3_2;
         div.innerHTML = serverAntwort;
     }
     async function clickJson() {
+        url = "https://marthgissose2021.herokuapp.com/";
         let formData = new FormData(document.forms[0]);
         let query = new URLSearchParams(formData);
         url = url + "/json?" + query.toString();
@@ -29,10 +25,6 @@ var Aufgabe3_2;
         let serverAntwort = await response.text();
         console.log(serverAntwort);
         console.log("antwort erhalten");
-        div.innerHTML = serverAntwort;
     }
-    function test() {
-        console.log("hallo");
-    }
-})(Aufgabe3_2 = exports.Aufgabe3_2 || (exports.Aufgabe3_2 = {}));
+})(Aufgabe3_2 || (Aufgabe3_2 = {}));
 //# sourceMappingURL=scriptserver.js.map
--- Aufgabe3.2/scriptserver.ts
+++ Aufgabe3.2/scriptserver.ts
@@ -1,15 +1,11 @@
-
-export namespace Aufgabe3_2 {
-    document.getElementById("html").addEventListener("click", test);
+namespace Aufgabe3_2 {
     document.getElementById("html").addEventListener("click", clickHtml);
     document.getElementById("json").addEventListener("click", clickJson);
-    //htmlButton.addEventListener("click", handleHTML);
-    //jsonButton.addEventListener("click", handleJSON);
-    let url: string = "https://marthgissose2021.herokuapp.com/";
-    let div: HTMLDivElement = <HTMLDivElement>document.createElement("div");
-    document.body.appendChild(div);  
+    let url: string;
+    let div: HTMLDivElement = <HTMLDivElement>document.getElementById("anzeige");
 
     async function clickHtml(): Promise<void> {
+        url = "https://marthgissose2021.herokuapp.com/";
         let formData: FormData = new FormData(document.forms[0]);
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "/html?" + query.toString();
@@ -21,6 +17,7 @@ export namespace Aufgabe3_2 {
     }
 
     async function clickJson(): Promise<void> {
+        url = "https://marthgissose2021.herokuapp.com/";
         let formData: FormData = new FormData(document.forms[0]);
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "/json?" + query.toString();
@@ -28,11 +25,6 @@ export namespace Aufgabe3_2 {
         let serverAntwort: string = await response.text();
         console.log(serverAntwort);
         console.log("antwort erhalten");
-        div.innerHTML = serverAntwort;
-    }
-    function test(): void {
-        console.log("hallo");
-        
     }
 
 }
